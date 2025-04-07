db68cbba8911a6bd32cd851aa48eb4e85c29b677
272799 <272799@vutbr.cz>
2025-04-07 14:17:08 +0200

    b

--- searching.py
+++ searching.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from setuptools.dist import sequence
+
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -21,12 +23,41 @@ def read_data(file_name, field):
         print(f"Field {field} not exist.")
         return None
 
+def pattern_search(sequence, pattern):
+    set_of_idxs = set()
+    pattern_length = len(pattern)
+    for idx in range(0, len(sequence) - pattern_length):
+        pattern_similarity = 0
+        for idx_pattern, pattern_element in enumerate(pattern):
+            if sequence [idx + idx_pattern] == pattern_element:
+                pattern_similarity = pattern_similarity + 1
+            else:
+                pass
+        if pattern_similarity == pattern_length:
+            set_of_idxs.add(idx + pattern_length // 2 - 1)
+        else:
+            pass
+    return set_of_idxs
+
+
+
+
 
 def main():
-    pass
+    my_data = read_data("sequential.json", "unordered_numbers")
+    print(my_data)
 
+def linear_search(sequence, number):
+    list_of_idxs = []
+    for idx, element in enumerate(sequence):
+        if element == number:
+            list_of_idxs.append(idx)
+        else:
+            continue
+    return {"positions":list_of_idxs, "count":len(list_of_idxs)}
 
 if __name__ == '__main__':
     main()
-    my_data = read_data("sequential.json", "unordered_numbers")
-    print(my_data)
+    my_list = [1, 2, 5, 7]
+    searched_number = 5
+    found_numbers = linear_search(my_list, searched_number)
